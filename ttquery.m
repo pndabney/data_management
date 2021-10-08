c63a20e76881f7ff384805d981527a73cebb18d9
Page Dabney <pdabney@cressida.princeton.edu>
2021-10-07 20:42:38 -0400

    Updates to method and now saves data in a text file

--- ttquery.m
+++ ttquery.m
@@ -1,49 +1,63 @@
-function varargout=ttquery(rlat,rlon,elat,elon,edepth)
-% [data]=ttquery(rlat,rlon,elat,elon,edepth)
+function varargout=ttquery(direc, model,phases,staloc,evloc,edepth)
+% [tt_query]=ttquery(direc,model,phases,staloc,evloc,edepth)
 %
-% Obtains a data table with P and S travel times from IRIS.
+% Creates a text file with traveltime data from IRIS.
 %
 % INPUT:
 %
-% rlat          Latitude of receiver 
-% rlon          Longitude of receiver
-% elat          Event latitude
-% elon          Event longitude
+% direc         Directory where the output file will be stored
+% model         1-D velocity model: 'prem', 'ak135','iasp91'
+% phases        Comma separated string list of phases (e.g. 'P,S,SKS,PKP')
+% staloc        [lat, lon] of the station(s)
+% evloc         [lat, lon] of the event
 % edepth        Event depth
 %
 % OUTPUT:
 %
-% data          Table with travel time data
+% tt_query      URL query (can entered in search bar)
 %
-% Last modified by pdabney@princeton.edu, 07/19/21
+% Last modified by pdabney@princeton.edu, 10/07/21
 
-% Initialize constant substrings
-inital = 'http://service.iris.edu/irisws/traveltime/1/query?';
-model = 'model=prem'; 
-phases = 'phases=S,P';
-sta = 'staloc='; 
+% Define default values
+defval('model', 'prem')
+defval('phases','P,S')
 
-% Reformat and join substrings
-locs = sprintf('[%.4f,%.4f] ',rlat,rlon); % required
-locIn = strrep(locs,' ',','); % reformat string
-loc = locIn(1:end-1); % remove extra comma at the end
-staloc = strcat(sta,loc);
-begin = strcat(inital,staloc);
+% To get traveltime data from IRIS Web Services
+tturl = 'http://service.iris.edu/irisws/traveltime/1/query?';
 
-evdepth = sprintf('evdepth=%.1f',edepth);
-evloc = sprintf('evloc=[%.4f,%.4f]',elat, elon); % required
+% Format event and station location input format
+eloc = sprintf('[%.4f,%.4f]',elat,elon);
 
-% List of all the substrings
-words = {begin,evloc,evdepth,phases,model};
+% Reformat staloc 
+% If there is more than one station inputed
+sz = size(staloc);
+if sz(1) == 1
+    rloc = sprintf('[%g,%g]',staloc(1),staloc(2);
+elseif sz(1) > 1
+    for i = 1:sz(1)
+        loc{i} = sprintf('[%g,%g]',staloc(i,1),staloc(i,2));
+    end
+    rloc = strjoin(loc,',');
+end
+
+% Format url event query 
+paramform = 'model=%s&phase=%s&evdepth=%g&evloc=%s&staloc=%s';
+myparam = sprintf(paramform, model, phase, edepth, eloc, rloc);
 
 % Join all the substrings
-query = strjoin(words,'&');
+tt_query = strcat(tturl,myparam);
 
 % Get travel time data from IRIS
-data = webread(query);
+query_output = webread(tt_query);
 
-% option for output
-varns={data};
+% Create text file to store traveltime info
+filename = 'TRAVELTIMES.txt';
+fileID = fopen(fullfile(direc,filename),'w');
+fprintf(fileID, query_output);
+fclose(fileID)
+
+% Option output
+varns={tt_query};
 varargout=varns(1:nargout);
 
 end
