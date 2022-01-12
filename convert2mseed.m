e48d2248912b6415e30eaa4056efd36b37ee87be
Page Dabney <pdabney@cressida.princeton.edu>
2022-01-12 14:41:24 -0500

    Added specific files to convert option

--- convert2mseed.m
+++ convert2mseed.m
@@ -1,20 +1,27 @@
-function convert2mseed(direc)
-% 
+function convert2mseed(direc, filenames)
+% convert2mseed(direc, filenames) 
+%
 % Converts all SAC files in a folder to mseed files.
 %
 % INPUT:
 % 
 % direc            Directory where SAC files are stored
+% filenames        String array of filenames
 %
 % NOTE:
 %
 % Requires sac2mseed. See https://github.com/iris-edu/sac2mseed for more details.
 %
-% Last modified by pdabney@princeton.edu, 1/5/22
+% Last modified by pdabney@princeton.edu, 01/12/22
 
-% Make command sequence
-% Go to specified location where the SAC files are stored and convert to mseed files
-tcom=sprintf('cd %s ; sac2mseed *', direc);
+% Make command sequence -  Go to specified location where the SAC files are stored and convert to mseed files
+switch nargin
+  case 2
+    files = sprintf(' %s',filenames);
+    tcom=sprintf('cd %s ; sac2mseed%s', direc, files);
+  case 1
+    tcom=sprintf('cd %s ; sac2mseed *', direc);
+end
 
 % Execute the command sequence
 system(sprintf('%s',tcom));
